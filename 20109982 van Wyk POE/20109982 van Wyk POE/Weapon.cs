b5381b838be84731341bd6a58bea1aaf54f6b730
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-12-01 14:18:35 +0200

    Q.2.1

---
+++ 20109982 van Wyk POE/20109982 van Wyk POE/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_van_Wyk_POE
+{
+    abstract class Weapon : Item
+    {
+        protected int damage { get; set; }
+        virtual protected int range { get; set; }
+        protected int durability { get; set; }
+        protected int cost { get; set; }
+        protected string weaponType { get; set; }
+
+        public Weapon(char symbol, int weaponXPos, int weaponYPos) : base(weaponXPos, weaponYPos)
+        {
+
+        }
+
+
+    }
+}
