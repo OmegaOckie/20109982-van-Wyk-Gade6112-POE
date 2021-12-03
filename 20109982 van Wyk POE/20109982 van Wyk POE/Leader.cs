aba36fd791409e55c33b46f4b088dfd3b2113050
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2021-12-03 16:36:27 +0200

    Q.2.4
    Did not understand what was required for the movement

---
+++ 20109982 van Wyk POE/20109982 van Wyk POE/Leader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_van_Wyk_POE
+{
+    class Leader : Enemy
+    {
+        private Tile enemyTarget { get; set; }
+
+        public Leader(int enemyXPos, int enemyYPos) : base (enemyXPos, enemyYPos, 2, 20, 'L')
+        {
+            
+        }
+
+        public override Movement ReturnMove(Movement move = Movement.NONE)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
